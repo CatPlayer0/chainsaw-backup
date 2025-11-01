859a93151ec672647838c008ed6cce8a3c0d39f3
catplayer8274 <dima.duduka@gmail.com>
2025-11-01 14:49:31 +0300

    indev

--- chainsaw-backup.py
+++ chainsaw-backup.py
@@ -1,1 +1,29 @@
-
+#declare
+from os import system
+from os import getcwd 
+import sys
+import subprocess
+from time import sleep
+#define
+target_dirs=[]
+backup_dir=''
+config=''
+#functions
+def kill():     #immediately terminate the program
+    sys.exit() 
+def help():     #dislpay help information
+    print('chainsaw-backup help:')
+def output(string):
+    return(subprocess.check_output(string, shell=True, text=True))
+#start up routine
+if output('silly-software/')=='bash: silly-software/: Is a directory':
+    if output('silly-software/chainsaw-backup')=='bash: silly-software/chainsaw-backup/: Is a directory':
+        if 'No such file or directory' not in output('cat silly-software/chainsaw-backup/config'):
+            print('All files intact')
+            config=open()
+else:
+    system('mkdir silly-software/')
+    system('mkdir silly-software/chainsaw-backup/')
+#mainloop
+while True:
+    inp=(input())
